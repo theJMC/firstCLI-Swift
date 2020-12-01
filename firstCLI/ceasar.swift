3b8a833c86e03b12dabd83f6cecdb5312e86dc5e
James McCarthy <james@jamesmcc.co.uk>
2020-12-01 12:13:15 +0000

    Created Cesar Cipher Scrit[

---
+++ firstCLI/ceasar.swift
@@ -0,0 +1,37 @@
+//
+//  ceasar.swift
+//  firstCLI
+//
+//  Created by James McCarthy on 01/12/2020.
+//
+
+import Foundation
+
+
+func ceasar() {
+//    ||=== Var Initialisation ===||
+    let alphabet = Array("abcdefghijklmnopqrstuvwxyz")
+    
+    print("Please enter the string to put into the Ceasar Cipher: ")
+    let target = readLine()!
+    
+    print("Please enter the shift amount: ")
+    let shift = Int(readLine()!)
+    
+//    Split target into an array
+    let array = Array(target.trimmingCharacters(in: .whitespacesAndNewlines))
+    
+//    Initialise the final resutling array.
+    var final = [String]()
+
+//  Iterate over the Array
+    for chara in array {
+        let shiftedChar = alphabet.firstIndex(of: chara)! + shift!
+        let finalShift = shiftedChar % alphabet.count
+        final.append(String(alphabet[finalShift]))
+    }
+    
+//    Print Final array
+    print(final.joined())
+}
+
---
+++ firstCLI/greeter.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  firstCLI
+//
+//  Created by James McCarthy on 01/12/2020.
+//
+
+import Foundation
+
+class Person {
+    var name = ""
+    var age = 0
+    func description() -> String {
+        if age == 1 {
+            return "Hello! I'm \(name), I'm a \(age) year old"
+        } else {
+            return "Hello! I'm \(name), I'm \(age) years old"
+        }
+    }
+    func greet() -> String {
+        return "Well Hello There \(name)"
+    }
+}
+
+func greet() {
+    print("What's your name?")
+    let name = readLine()
+    let p1 = Person()
+    p1.name = name!.capitalized
+    print("How old are you?")
+    let age = Int(readLine()!)
+    p1.age = age!
+
+    print(p1.description())
+}
