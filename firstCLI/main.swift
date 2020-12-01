57d882c97b0722dbbf53e8a6567fe620abb78d17
James McCarthy <james@jamesmcc.co.uk>
2020-12-01 11:23:10 +0000

    Created a Hello and Greeter Class testing

--- firstCLI/main.swift
+++ firstCLI/main.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
+class Person {
+    var name = ""
+    var age = 0
+    func description() -> String {
+        if age == 1 {
+            return "Hello! I'm \(name), I'm a \(age) year old"
+        } else {
+            return "Hello! I'm \(name), I'm \(age) years old"
+        }
+    }
+    func greet() -> String {
+        return "Well Hello There \(name)"
+    }
+}
+
+print("What's your name?")
+var name = readLine()
+var p1 = Person()
+p1.name = name!.capitalized
+print("How old are you?")
+var age = Int(readLine()!)
+p1.age = age!
+
+print(p1.description())
 
